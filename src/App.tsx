e68797455bbd559a64de3e85b656448f7c76cfb9
nyanlintun <nyanlintun28504@gmail.com>
2023-06-07 23:03:50 +0630

    second

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import { Routes, Route, Navigate } from "react-router-dom";
+import NewNote from "./screen/NewNote";
 
 const App = () => {
   let data: string = "Nyan Lin Tun";
-
+  const dd = "dddd";
   return (
-    <div>
-      <button>Click</button>
-    </div>
+    <Container>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/new" element={<NewNote />} />
+        <Route path="/:id">
+          <Route index element={<h1>Show</h1>} />
+          <Route path="edit" element={<h1>Edit</h1>} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Container>
   );
 };
 
